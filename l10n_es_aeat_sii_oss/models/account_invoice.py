824a6542f495b5c8f5e2597261f080b2b9821a6e
Omar (Comunitea) <omar@comunitea.com>
2021-06-30 21:41:29 +0200

    [MIG] l10n_es_aeat_sii_oss

--- l10n_es_aeat_sii_oss/models/account_invoice.py
+++ l10n_es_aeat_sii_oss/models/account_invoice.py
@@ -1,14 +1,15 @@
+# -*- coding: utf-8 -*-
 # Copyright 2021 FactorLibre - Rodrigo Bonilla <rodrigo.bonilla@factorlibre.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from openerp import api, models
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     def _get_sii_map_lines_code_apply(self):
-        return ['SFESB']
+        return ['SFENS', 'NotIncludedInTotal']
 
     @api.multi
     def _get_sii_taxes_map(self, codes):
@@ -18,7 +19,7 @@ class AccountInvoice(models.Model):
         :param codes: List of code strings to get the mapping.
         :return: Recordset with the corresponding codes
         """
-        taxes = super()._get_sii_taxes_map(codes)
+        taxes = super(AccountInvoice, self)._get_sii_taxes_map(codes)
         sii_map_lines_code_apply = self._get_sii_map_lines_code_apply()
         if any([x for x in codes if x in sii_map_lines_code_apply]):
             taxes |= self.env['account.tax'].search([
