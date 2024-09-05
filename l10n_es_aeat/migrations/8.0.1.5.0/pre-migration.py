a42e918ab9b14ee66b0ef661ec856ab652782c11
javierjcf <javier@comunitea.com>
2024-09-05 18:07:53 +0200

    [FIX]Corregir migrations innecesario

--- l10n_es_aeat/migrations/8.0.1.5.0/pre-migration.py
+++ l10n_es_aeat/migrations/8.0.1.5.0/pre-migration.py
@@ -2,15 +2,15 @@
 # © 2016 - Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
+# En prodimar la columna legacy_report_id ya existe.
 def migrate(cr, version):
     if not version:
         return
-    cr.execute("SELECT 1 FROM pg_class WHERE relname='l10n_es_aeat_tax_line'")
-    if not cr.fetchone():
-        return
-    cr.execute(
-        """
-        ALTER TABLE l10n_es_aeat_tax_line
-        RENAME report TO legacy_report_id
+    # cr.execute("SELECT 1 FROM pg_class WHERE relname='l10n_es_aeat_tax_line'")
+    # if not cr.fetchone():
+    #     return
+    # cr.execute(
+    #     """
+    #     ALTER TABLE l10n_es_aeat_tax_line
+    #     RENAME report TO legacy_report_id
         """)
