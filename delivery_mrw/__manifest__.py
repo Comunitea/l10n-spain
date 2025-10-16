0a0fa0d461688f76b02141b31522723ad1d62a86
Vicente <vicente@comunitea.com>
2025-10-16 12:36:05 +0200

    [FIX] delivery_mrw: stock.inmediate.transfer no existe ya

--- delivery_mrw/__manifest__.py
+++ delivery_mrw/__manifest__.py
@@ -18,7 +18,6 @@
         "views/delivery_mrw_view.xml",
         "views/stock_picking_views.xml",
         "views/mrw_manifest_template.xml",
-        "wizard/stock_immediate_transfer_views.xml",
         "wizard/mrw_manifest_wizard_views.xml",
         "data/delivery_mrw.xml",
         "security/ir.model.access.csv",
